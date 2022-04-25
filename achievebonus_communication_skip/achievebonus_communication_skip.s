beb7659d6be568bd1e544b8aa69c74d873f25e7a
Muh Muhten <muh.muhten@gmail.com>
2022-04-25 02:06:08 -0400

    Import bonus box patch

---
+++ achievebonus_communication_skip/achievebonus_communication_skip.s
@@ -0,0 +1,8 @@
+.3ds
+.open "code.bin","out.bin",0x100000
+.include "ident.s"
+
+.org _SYM_.castle__anonymous_namespace__AchieveBonusMenuItem__GetType + 48
+	b	_SYM_.castle__anonymous_namespace__AchieveBonusMenuItem__GetType + 220
+
+.close
