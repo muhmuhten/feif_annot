0e33639ae3f965f0623360ba65e9dd1430af94b6
Muh Muhten <muh.muhten@gmail.com>
2022-04-26 10:24:40 -0400

    Import capture skill skip

---
+++ capture_skill_skip/capture_skill_skip.s
@@ -0,0 +1,8 @@
+.3ds
+.open "code.bin","out.bin",0x100000
+.include "autosym.s"
+
+.org _SYM_.map__anonymous_namespace__Capture__BuildAttribute + 0x28
+	b	_SYM_.map__anonymous_namespace__Capture__BuildAttribute + 0x48
+
+.close
